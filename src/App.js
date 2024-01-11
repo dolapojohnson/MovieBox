c8ec04b5966f074c81d37cafc52a64bc82f67434
dolapo <dolapo.johnson@afripointgroup.com>
2024-01-11 13:03:59 +0100

    pre-carousel commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.scss';
 import Header from './components/Header';
+import MovieCard from './components/MovieCard';
 
 
 const App = () => {
   return(
     <div className='app'>
       <Header />
+      <MovieCard />
     </div>
   )
 }
